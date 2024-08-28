298c600ee0cd7c3ade279a53d7940ef5d8456afc
Marcus Santos <marcus.silva@sisconsultoria.com.br>
2024-08-28 08:19:35 -0400

    Commit inicial

---
+++ src/main/java/com/example/api_infuse_pedidos/ApiInfusePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_infuse_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInfusePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInfusePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_infuse_pedidos/ApiInfusePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_infuse_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInfusePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
